2cf177ac2b7b32c32c20519382dac94a9d767b12
thor <thor@gmail.com>
2024-11-19 09:32:18 +0530

    files added

---
+++ k8s_commands.sh
@@ -0,0 +1,8 @@
+kubectl apply -f ./k8s_manifest.yml
+kubectl delete -f ./k8s_manifest.yml
+
+kubectl apply -f ./k8s_manifest.yml.yml
+
+kubectl get pods
+kubectl describe pod my-pod   (complete details)
+kubectl edit
